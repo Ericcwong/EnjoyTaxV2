bcbfe283bf5fbc1582442790778d4f604760008e
Eric Wong <ericwong12@live.com>
2023-07-12 22:31:48 -0600

    Updates

--- src/Components/Dashboard.tsx
+++ src/Components/Dashboard.tsx
@@ -1,7 +1,24 @@
+import { CardPaymentDetails } from 'square'
+import { Client, Environment, ApiError } from 'square'
+
 export default function Dashboard() {
+  const client = new Client({
+    accessToken: import.meta.env.VITE_SQUARE_ACCESS_TOKEN,
+    environment: Environment.Production,
+  })
+  const { locationsApi } = client
+  async function getTransactions() {
+    try {
+      let listTransactionResponse = await client.paymentsApi.listPayments()
+      console.log(listTransactionResponse)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+  getTransactions()
   return (
-    <>
+    <div className="xl:container xl:m-auto h-full">
       <h1>Dashboard</h1>
-    </>
+    </div>
   )
 }
